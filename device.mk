e6b249395a511661ca14637681f37c46f666a757
Andre Eisenbach <eisenbach@google.com>
2017-12-06 16:16:36 -0800

    sfo: Rename libnfc-brcm.conf -> libnfc-nci.conf
    Test: manual
    Merged-In: I9655a14e021024540cf342022cb817e1f8d0f9ad
    Change-Id: I9655a14e021024540cf342022cb817e1f8d0f9ad

--- device.mk
+++ device.mk
@@ -111,8 +111,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/vendor/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/vendor/etc/permissions/android.hardware.nfc.xml \
-    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/nfc/libnfc-brcm-20795a10.conf:system/etc/libnfc-brcm-20795a10.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nci-20795a10.conf:system/etc/libnfc-nci-20795a10.conf \
     $(LOCAL_PATH)/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # Proprietery Firmware
