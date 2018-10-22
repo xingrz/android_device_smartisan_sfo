2a71fac955225486f08e25ec4ed32543e95ec68b
XiNGRZ <chenxingyu92@gmail.com>
2018-10-23 03:44:09 +0800

    HAX: CMify

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,9 +86,9 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 
 # MK Hardware
 BOARD_HARDWARE_CLASS += \
-    hardware/mokee/mkhw \
+    hardware/cyanogen/mkhw \
     $(DEVICE_PATH)/mkhw
-BOARD_USES_MOKEE_HARDWARE := true
+BOARD_USES_CYANOGEN_HARDWARE := true
 
 # CNE and DPM
 BOARD_USES_QCNE := true
