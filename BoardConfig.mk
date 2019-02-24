0468febf69bbae8e639ff4e866f13a52591bbf59
XiNGRZ <chenxingyu92@gmail.com>
2019-02-24 16:08:24 +0800

    sfo: Enable TARGET_CPU_SMP
    Change-Id: I3fc217c93c4f0dbdd93aed6e5de89edf8cf6560e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := krait
+TARGET_CPU_SMP := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
