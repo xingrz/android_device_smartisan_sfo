04c1e4068926b0d600f7783df92a863b8bfc307e
XiNGRZ <chenxingyu92@gmail.com>
2019-02-25 15:03:30 +0800

    sfo: Move WCNSS_qcom_cfg.ini to /vendor
    Change-Id: I3d82f4a976414e46418682b15dae8077e1c4affa

--- device.mk
+++ device.mk
@@ -282,4 +282,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_ath6kl.conf:system/etc/wifi/wpa_supplicant_ath6kl.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
