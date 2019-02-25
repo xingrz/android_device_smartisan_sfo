77eecd069a8ad870336ef53e67f72e895d25eb8c
XiNGRZ <chenxingyu92@gmail.com>
2019-02-25 09:48:29 +0800

    sfo: Move seccomp configs to /vendor
    Change-Id: I39f72094be69ea010f8b6ff3d989a9fc7dce2bee

--- device.mk
+++ device.mk
@@ -246,8 +246,8 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_SYSTEM)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_SYSTEM)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # Sensor
 PRODUCT_PACKAGES += \
