7e0af35054580883cfd6f6528d311f9eff20d997
Bruno Martins <bgcngm@gmail.com>
2019-10-04 23:20:42 +0100

    sfo: Switch to legacy Wi-Fi service
    Change-Id: I690bc80349e47907a29a89b8a6f96659f1c61aba

--- device.mk
+++ device.mk
@@ -82,7 +82,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/vendor/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     libwpa_client \
     hostapd \
     wificond \
